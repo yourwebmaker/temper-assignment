305309d6bc2caa947da83ee24fba8dfcd40b6d65
Daniel Lima <yourwebmaker@gmail.com>
2019-11-26 18:26:00 +0100

    Dirty PHP solution in one file. I will split into classes and methods later or for better testability, maintainability and readability.

---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php
+$csvRows = file(__DIR__ . '/../data/export.csv');
+$data = [];
+foreach ($csvRows as $i => $csvRow) {
+    //todo add tests
+    if ($i === 0) {
+        continue;
+    }
+
+    [$userId, $dateCreatedTime, $step] = str_getcsv($csvRow, ';');
+
+    $weekNumber = (DateTimeImmutable::createFromFormat('Y-m-d', $dateCreatedTime))->format('W');
+    $weekKey = 'week ' . $weekNumber;
+
+    //todo add tests
+    if (isset($data[$weekKey]['total'])) {
+        $data[$weekKey]['total']++;
+    } else {
+        $data[$weekKey]['total'] = 1;
+    }
+
+    //todo add tests
+    //todo stepname
+    if (isset($data[$weekKey]['steps'][$step]['total'])) {
+        $data[$weekKey]['steps'][$step]['total']++;
+    } else {
+        $data[$weekKey]['steps'][$step]['total'] = 1;
+    }
+}
+
+$mappedData = array_map(static function ($week) {
+    ksort($week['steps']);
+    return array_map(static function ($step) use ($week){
+        $step['percentage'] = ($step['total'] /  $week['total']) * 100;
+        return $step;
+    }, $week['steps']);
+}, $data);
+
+$chartData = [];
+foreach ($mappedData as $weekName => $week) {
+    $chartData[$weekName] = [
+        'name' => $weekName,
+    ];
+
+    foreach ($week as $stepName => $step) {
+        $chartData[$weekName]['data'][] = [(int) $stepName, (float) $step['percentage']];
+    }
+}
+
+echo json_encode(array_values($chartData));
